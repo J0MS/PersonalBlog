2051afd94fdcbf3d9b086bc24ccb209e50b97aab
@JOMS <passwd@ciencias.unam.mx>
2019-09-08 19:45:28 -0500

    Adding class based views

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic import View
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
@@ -8,9 +8,23 @@ def index(request):
     template = loader.get_template('Home/index.html')
     return HttpResponse( template.render({}, request))
 
+class Index(View):
+    """
+        Index in my Web Page but with Clased based views.
+    """
+    template = 'Home/index.html'
+    # template = loader.get_template('Home/index.html')
+    context = {'title': 'Index'}
 
-def Home(request):
-    context = {"message":"hola"}
-  #  template = loader.get_template('/cover/index.html')
-    return render(request, '/Home/index.html', context)
-    #return HttpResponse(template.render(request))
+    def get(self, request):
+        """
+            Get in my Index.
+        """
+        print('With class-based views')
+        return render(request, self.template, self.context)
+
+# def Home(request):
+#     context = {"message":"hola"}
+#   #  template = loader.get_template('/cover/index.html')
+#     return render(request, '/Home/index.html', context)
+#     #return HttpResponse(template.render(request))
