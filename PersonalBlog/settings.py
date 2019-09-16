c86c6b56932a0567ff308cd6526853267926c499
@JOMS <passwd@ciencias.unam.mx>
2019-09-15 20:31:29 -0500

    Update structure

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'Home.apps.HomeConfig'
+    'Home',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static'),
+]
+ 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
